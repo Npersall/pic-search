9c04c90aec630c9da80ca8cfc0871f3d43b0d6fb
npersall <npersall@gmail.com>
2020-07-08 17:28:55 -0700

    Removes API key reset- key was reset after removal.

--- src/api/unsplash.js
+++ src/api/unsplash.js
@@ -3,6 +3,6 @@ import axios from 'axios';
 export default axios.create({
   baseURL: 'https://api.unsplash.com',
   headers: {
-    Authorization: 'Client-ID DlMI9Rjc-fTp61aRF5n5L8BYoGJDzN37IVhJuXa4jeM'
+    Authorization: 'Client-ID API_KEY_GOES_HERE'
   }
 });
